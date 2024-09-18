0ac3ff71c6089a9644e885df6d5e8c7a8e0ad848
Emad <emadmoome@gmail.com>
2024-09-18 22:44:57 +0300

    add: src

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/clerk-react";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
