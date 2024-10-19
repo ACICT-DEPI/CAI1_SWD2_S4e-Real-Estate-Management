f29b65a0e348b50d26b22908e7e1bfcc3b627cb2
a-edrees <a.edrees@solutionplus.net>
2024-10-19 04:57:08 +0300

    fix: header is now responsive

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import logo from "../../assets/Logo.svg";
-import { Button } from "antd";
+import logo from "../../assets/Logo2.svg";
+import { Button, Drawer } from "antd";
 import {
   SignIn,
   SignUp,
@@ -12,10 +12,27 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Store } from "lucide-react";
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   const [showSignUp, setShowSignUp] = useState(false);
   const [showSignIn, setShowSignIn] = useState(false);
   const location = useLocation();
+
+  // handle drawer
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  const drawerSignIn = () => {
+    setOpen(false);
+    setShowSignIn(true);
+  };
+  const drawerSignUp = () => {
+    setOpen(false);
+    setShowSignUp(true);
+  };
 
   const handleOverlay = (e) => {
     if (e.target === e.currentTarget) {
@@ -37,7 +54,7 @@ const Navbar = () => {
   }, [location.search]);
   return (
     <>
-      <div>
+      <div className="shadow-md mb-10 lg:mb-0">
         <nav className="flex items-center justify-between me-4 border-none">
           {/* Logo */}
           <div className="flex items-center">
@@ -46,51 +63,89 @@ const Navbar = () => {
             </Link>
           </div>
 
-          {/* Hamburger */}
-          <div
-            className="md:hidden flex flex-col cursor-pointer"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            <div className="w-8 h-1 bg-gray-600 mb-1"></div>
-            <div className="w-8 h-1 bg-gray-600 mb-1"></div>
-            <div className="w-8 h-1 bg-gray-600"></div>
-          </div>
+          {/* drawer */}
+          <Drawer onClose={onClose} closable={false} width="60%" open={open}>
+            <Link to="/" onClick={onClose}>
+              <img
+                src={logo}
+                alt="Company Logo"
+                width={160}
+                className="mx-auto"
+              />
+            </Link>
 
+            <div className="flex flex-col items-center justify-center gap-3">
+              <Link
+                to="/"
+                className="text-gray-600 hover:text-white hover:bg-violet-500 w-full text-center p-2 rounded font-bold"
+                aria-label="Home"
+                onClick={onClose}
+              >
+                Home
+              </Link>
+              <Link
+                to="/About"
+                className="text-gray-600 hover:text-white hover:bg-violet-500 w-full text-center p-2 rounded font-bold"
+                aria-label="About"
+                onClick={onClose}
+              >
+                About
+              </Link>
+              <Link
+                to="/ContactUs"
+                className="text-gray-600 hover:text-white hover:bg-violet-500 w-full text-center p-2 rounded font-bold"
+                aria-label="Contact"
+                onClick={onClose}
+              >
+                Contact Us
+              </Link>
+              <SignedIn>
+                <Link to="/AddProperty">
+                  <Button
+                    type="Dashed  "
+                    className=" font-semibold shadow text-white rounded-full w-24 h-9 bg-violet-700 hover:bg-violet-500 "
+                    style={{ fontSize: "11px" }}
+                    onClick={onClose}
+                  >
+                    Add Property
+                  </Button>
+                </Link>
+              </SignedIn>
+              <SignedOut>
+                <Button
+                  variant="outline"
+                  className=" font-semibold shadow rounded-full w-28 h-9 "
+                  onClick={drawerSignUp}
+                >
+                  SignUp
+                </Button>
+                <Button
+                  type="Dashed  "
+                  className="  font-semibold shadow text-white rounded-full w-28 h-9 bg-violet-700 hover:bg-violet-500 "
+                  onClick={drawerSignIn}
+                >
+                  SignIn
+                </Button>
+              </SignedOut>
+            </div>
+          </Drawer>
           {/* Links */}
-          <div
-            className={`md:flex ${
-              isMenuOpen ? "block mt-20 md:mt-0 " : "hidden mt-0"
-            } absolute md:static  w-full md:w-auto z-10`}
-          >
-            <ul className="flex flex-col bg-gray-100 md:flex-row space-y-4 md:space-y-0 md:space-x-10 p-4 md:p-0">
-              <li className="flex">
+          <div className={`md:flex absolute md:static  w-full md:w-auto z-10`}>
+            <ul className="hidden bg-gray-100 md:flex space-y-4 md:space-y-0 lg:space-x-10 p-4 md:p-0">
+              <li>
                 <Link
                   to="/"
-                  className="text-gray-600 hover:text-indigo-500"
+                  className="text-gray-600 font-bold hover:text-white hover:bg-violet-700 rounded py-2 px-6"
                   aria-label="Home"
                 >
                   Home
                 </Link>
-                <p
-                  className="ms-64 md:hidden sm:block "
-                  onClick={() => setIsMenuOpen(!isMenuOpen)}
-                >
-                  &#9932;
-                </p>
               </li>
-              {/* <li>
-                <Link
-                  to="/property"
-                  className="text-gray-600 hover:text-indigo-500"
-                  aria-label="Property"
-                >
-                  Property
-                </Link>
-              </li> */}
+
               <li>
                 <Link
                   to="/About"
-                  className="text-gray-600 hover:text-indigo-500"
+                  className="text-gray-600 font-bold hover:text-white hover:bg-violet-700 rounded py-2 px-6"
                   aria-label="About"
                 >
                   About
@@ -99,7 +154,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/ContactUs"
-                  className="text-gray-600 hover:text-indigo-500"
+                  className="text-gray-600 font-bold hover:text-white hover:bg-violet-700 rounded py-2 px-6"
                   aria-label="Contact"
                 >
                   Contact Us
@@ -113,24 +168,24 @@ const Navbar = () => {
             <SignedOut>
               <Button
                 variant="outline"
-                className=" font-semibold shadow rounded-full w-28 h-9 "
+                className=" font-semibold shadow rounded-full w-28 h-9 hidden md:block"
                 onClick={() => setShowSignUp(true)}
               >
                 SignUp
               </Button>
               <Button
                 type="Dashed  "
-                className="  font-semibold shadow text-white rounded-full w-28 h-9 bg-indigo-500 hover:bg-indigo-600 "
+                className="  font-semibold shadow text-white rounded-full w-28 h-9 bg-violet-700 hover:bg-violet-500 hidden md:block"
                 onClick={() => setShowSignIn(true)}
               >
                 SignIn
               </Button>
             </SignedOut>
             <SignedIn>
-              <Link to="/AddProperty">
+              <Link to="/AddProperty" className="hidden md:block">
                 <Button
                   type="Dashed  "
-                  className=" font-semibold shadow text-white rounded-full w-24 h-9 bg-indigo-500 hover:bg-indigo-600 "
+                  className=" font-semibold shadow text-white rounded-full w-24 h-9 bg-violet-700 hover:bg-violet-500 "
                   style={{ fontSize: "11px" }}
                 >
                   Add Property
@@ -152,6 +207,16 @@ const Navbar = () => {
                 </UserButton.MenuItems>
               </UserButton>
             </SignedIn>
+
+            {/* Hamburger */}
+            <div
+              className="md:hidden flex flex-col cursor-pointer"
+              onClick={showDrawer}
+            >
+              <div className="w-8 h-1 bg-violet-600 mb-1"></div>
+              <div className="w-8 h-1 bg-violet-600 mb-1"></div>
+              <div className="w-8 h-1 bg-violet-600"></div>
+            </div>
           </div>
         </nav>
       </div>
